8e3e6c7092b7009b1ff135c299b08efe856d40be
au573585 <201611672@post.au.dk>
2018-08-30 11:54:42 +0200

    initial

---
+++ repo/Calculator/Calculator.Application/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            Console.WriteLine("TESTS for Add");
+            Console.WriteLine($"2+2 = {calc.Add(2, 2)}");
+            Console.WriteLine($"2-3 = {calc.Add(2, -3)}");
+            Console.WriteLine($"-4-4 = {calc.Add(-4, -4)}");
+            Console.WriteLine($"-2+2 = {calc.Add(-2, 2)}\n");
+
+            Console.WriteLine("TESTS for subtract");
+            Console.WriteLine($"2-2 = {calc.Subtract(2, 2)}");
+            Console.WriteLine($"2-(-2) = {calc.Subtract(2, -2)}");
+            Console.WriteLine($"-12-2 = {calc.Subtract(-12, 2)}\n");
+
+            Console.WriteLine("TESTS for multiply");
+            Console.WriteLine($"2*2 = {calc.Multiply(2, 2)}");
+            Console.WriteLine($"-2*2 = {calc.Multiply(-2, 2)}");
+            Console.WriteLine($"0*(-2) = {calc.Multiply(0, -2)}");
+            Console.WriteLine($"-10*(-10) = {calc.Multiply(-10, -10)}\n");
+
+            Console.WriteLine("TESTS for power");
+            Console.WriteLine($"2^2 = {calc.Power(2, 2)}");
+            Console.WriteLine($"2^(-2) = {calc.Power(2, -2)}");
+            Console.WriteLine($"-2^2 = {calc.Power(-2, 2)}");
+            Console.WriteLine($"10^2 = {calc.Power(10, 2)}");
+        }
+    }
+}
---
+++ repo/Calculator/Test_calculator/test.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+
+
+class BankAccount 
+{
+private:
+	int balance;
+};
+
+TEST(AddTest, AddingTwoPostiveNumbers)
+{
+	BankAccount account;
+	EXPECT_EQ(0,account)
+}
