674e76f8e3c50563051cd6192ceec6e94ce86eb9
au545675 <201610466@post.au.dk>
2018-09-06 10:32:37 +0200

    Added Accumulator and Clear functions

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -3,31 +3,42 @@
 namespace Calculator  
 {
     public class Calculator
-    { 
+    {
+        public double Accumulator { get; private set; }
 
         public double Add(double a, double b)
         {
+            Accumulator = a + b;
             return a + b;
         }
 
         public double Subtract(double a, double b)
         {
+            Accumulator = a + b;
             return a - b;
         }
 
         public double Multiply(double a, double b)
         {
+            Accumulator = a * b;
             return a * b;
         }
 
         public double Power(double x, double exp)
         {
+            Accumulator = Math.Pow(x, exp);
             return Math.Pow(x, exp);
         }
         public double Divide(double dividend, double divisor)
         {
+            Accumulator = dividend / divisor;
             return dividend / divisor;
         }
-        public double Accumulator { get; private set; }
+
+        public void Clear()
+        {
+            Accumulator = 0;
+        }
+
     }
 }
